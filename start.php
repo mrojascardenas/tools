eb3b231a5e48349dc897db1d85e21a1cbddb7fae
mrojascardenas <mrojascardenas773@gmail.com>
2024-06-15 11:47:29 -0600

    Create start.php

---
+++ start.php
@@ -0,0 +1,17 @@
+selcet * from 
+<?php
+$con = new conexion "127.0.0.0.1","localhost","mysqli_query","acceskey"
+//* cheked
+select * where * from  onloadstart
+//* cheked
+select* where * from
+onoffiline(onpreogress),"acceskey"
+$mysqli_query
+//* cheked
+select * where * from
+onselect("mysqli_query","acceskey","127.0.0.0.1",localhost") UPGRADE SET
+// * cheked
+Start
+?>
+
+
